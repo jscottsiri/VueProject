050461a388685ae41d939deafd2011e792f53934
jscottsiri <jss82@njit.edu>
2019-05-08 20:36:50 -0400

    Methods and Likes\Dislikes

--- site.js
+++ site.js
@@ -33,15 +33,80 @@ const vue_app = new Vue({
       data: {
             // This holds your movies.json data.
             movies: [],
-			title: "IMDB + Scott's Top 8 Movies",
-			owner: "Scott",
-			github: "https://github.com/jscottsiri/VueProject"
-
+			
+			title: "IMBD + Scott's Top 9 Movies",
+            owner: "Scott",
+            github: "https://github.com/jscottsiri/"
             /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
+            
       },
       methods: {
             /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
+          makeTextDate: function (dateArray) {
+              let obj = new Object();
+              obj.year = dateArray[0];
+              obj.day = dateArray[2];
+              switch (dateArray[1]) {
+                  case 1:
+                      obj.month = 'January';
+                      break;
+                  case 2:
+                      obj.month = 'February';
+                      break;
+                  case 3:
+                      obj.month = 'March';
+                      break;
+                  case 4:
+                      obj.month = 'April';
+                      break;
+                  case 5:
+                      obj.month = 'May';
+                      break;
+                  case 6:
+                      obj.month = 'June';
+                      break;
+                  case 7:
+                      obj.month = 'July';
+                      break;
+                  case 8:
+                      obj.month = 'August';
+                      break;
+                  case 9:
+                      obj.month = 'September';
+                      break;
+                  case 10:
+                      obj.month = 'October';
+                      break;
+                  case 11:
+                      obj.month = 'November';
+                      break;
+                  case 12:
+                      obj.month = 'December';
+                      break;
+              }
+              return obj.month + " " + obj.day + ", " + obj.year + " ";
+          },
+          like: function (index) {
+              this.movies[index].likes++;
+          },
+          dislike: function (index) {
+              this.movies[index].dislikes++;
+          },
+          posterClick: function (index) {
+
+              if (this.movies[index].posterindex === this.movies[index].posters.length - 1) {
+                  this.movies[index].posterindex = 0;
+              }else{
+                  this.movies[index].posterindex++;
+              }
+
+          },
+          timeText: function (minutes) {
+              let min = minutes % 60;
+              let hr = Math.floor(minutes/60);
+
+              return hr + "h " + min + "m";
+          }
       }
 })
-	
 
